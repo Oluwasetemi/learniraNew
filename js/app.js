1a5154b4d65d5653e2ea01fcc5e293be753be510
OjoOluwasetemi <osojo@student.lautech.edu.ng>
2016-08-25 10:03:43 +0100

    add youtube content to startLearning.html

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-$(document).ready(function () {
-  $('.carousel').carousel({
-    interval: 5000
-  });
-});
+$( '.child' ).hide();
+$( document ).ready( function () {
+
+	$( '.maths' ).click( function () {
+		$( '.math-child' ).show();
+		$( '#set' ).click( function () {
+			$( '.content' ).append( '<iframe width="653" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjcCe0eygquHnjgQ2Bfli0gf" frameborder="0" allowfullscreen></iframe>' );
+		} )
+		$( '.surd' ).click( function () {
+			$( '.content' ).append( '<iframe width="653" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjf44XiMvtMhj4VFmrJp7IsP" frameborder="0" allowfullscreen></iframe>' );
+		} )
+	} );
+	$( '.wassce' ).click( function () {
+		$( '.wassce-child' ).show();
+	} );
+	$( '.cowbell' ).click( function () {
+		$( '.cowbell-child' ).show();
+	} );
+
+} );
