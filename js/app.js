d7f51cf47e986f6a9778677a4976c318950b0f91
OjoOluwasetemi <osojo@student.lautech.edu.ng>
2016-08-25 10:47:06 +0100

    add wassce and cowbell math video section to startLearning.html page

--- js/app.js
+++ js/app.js
@@ -18,12 +18,31 @@ $( document ).ready( function () {
 		$( '#trig' ).click( function () {
 			$( '.content' ).html( '<iframe width="853" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjfPXWYItOHByEqGzgzPrQLe" frameborder="0" allowfullscreen></iframe>' );
 		} )
+		$( '#sin' ).click( function () {
+			$( '.content' ).html( '<iframe width="853" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjfPXWYItOHByEqGzgzPrQLe" frameborder="0" allowfullscreen></iframe><iframe width="853" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjdie2D8_V1HJPqaSq7O4KA8" frameborder="0" allowfullscreen></iframe>' );
+		} )
+		$( '#cos' ).click( function () {
+			$( '.content' ).html( '<iframe width="853" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjdIjMpxrl77Z4BRgQrUfTrt" frameborder="0" allowfullscreen></iframe>' );
+		} )
+
 	} );
 	$( '.wassce' ).click( function () {
 		$( '.wassce-child' ).show();
+		$( '#2014' ).click( function () {
+			$( '.content' ).html( '<iframe width="853" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjdIjMpxrl77Z4BRgQrUfTrt" frameborder="0" allowfullscreen></iframe>' );
+		} )
+		$( '#2015' ).click( function () {
+			$( '.content' ).html( '<iframe width="853" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjck-0MD0_zbCpIj8tE7D4XR" frameborder="0" allowfullscreen></iframe>' );
+		} )
+		$( '#2016' ).click( function () {
+			$( '.content' ).html( '<iframe width="853" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjduaa3TohAl5BzzgSZ_N1uV" frameborder="0" allowfullscreen></iframe>' );
+		} )
 	} );
 	$( '.cowbell' ).click( function () {
 		$( '.cowbell-child' ).show();
+		$( '#MAR2016' ).click( function () {
+			$( '.content' ).html( '<iframe width="853" height="480" src="https://www.youtube.com/embed/videoseries?list=PL2jJTGkTmCjd3Mr95Rmq9BGbtD-8t-6Da" frameborder="0" allowfullscreen></iframe>' );
+		} )
 	} );
 
 } );
